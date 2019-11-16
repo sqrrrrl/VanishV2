94b8ba5da64c13f26a4e0ceb0957b600b65bc0fd
sqrrl <48035361+superbobby2000@users.noreply.github.com>
2019-11-16 13:56:57 -0500

    Fixing major bugs

--- src/superbobby/VanishV2/VanishV2Task.php
+++ src/superbobby/VanishV2/VanishV2Task.php
@@ -12,7 +12,7 @@ class VanishV2Task extends Task {
     public function onRun(int $currentTick){
         foreach(Server::getInstance()->getOnlinePlayers() as $p){
             if($p->spawned){
-                if(in_array($p->getName(), Vanish::$vanish)){
+                if(in_array($p->getName(), VanishV2::$vanish)){
                     foreach(Server::getInstance()->getOnlinePlayers() as $player){
 			if($player->hasPermission("vanish.see")){
 			  $player->showPlayer($p);
