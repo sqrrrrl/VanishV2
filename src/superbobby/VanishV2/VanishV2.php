5249c01755de72378072a5c185719fc3bbd3f4c2
manxy <mdemars99@gmail.com>
2020-01-05 05:48:53 -0500

    Some useless formatting and grammer fixes/changes.

--- src/superbobby/VanishV2/VanishV2.php
+++ src/superbobby/VanishV2/VanishV2.php
@@ -5,6 +5,7 @@ namespace superbobby\VanishV2;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat as C;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\network\mcpe\protocol\PlayerListPacket;
@@ -18,7 +19,7 @@ use function strtolower;
 use function sendFullPlayerListData;
 
 class VanishV2 extends PluginBase {
-    public const PREFIX = "§9Vanish §8» §r";
+    public const PREFIX = C::BLUE . "Vanish " . C::DARK_GRAY . "» ". C::RESET;
 
     public static $vanish = [];
 
@@ -42,47 +43,50 @@ class VanishV2 extends PluginBase {
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{
         $name = $sender->getName();
-        if(strtolower($cmd->getName()) === "vanish" or ($cmd->getName()) === "v"){
-	        if(!$sender instanceof Player){
-                $sender->sendMessage(self::PREFIX."§4Use this command InGame");
-                return false;
-            }
+		switch(strtolower($cmd->getName())) {
+			case "vanish":
+			case "v":
+				if(!$sender instanceof Player) {
+					$sender->sendMessage(self::PREFIX . C::DARK_RED . "You can only use this command in-game.");
+					return false;
+				}
 
-	        if(!$sender->hasPermission("vanish.use")){
-		        $sender->sendMessage(self::PREFIX."§4You do not have permission to use this command");
-                return false;
-            }
+				if(!$sender->hasPermission("vanish.use")) {
+					$sender->sendMessage(self::PREFIX . C::DARK_RED . "You do not have permission to use this command.");
+					return false;
+				}
 
-            if(!in_array($name, self::$vanish)){
-                self::$vanish[] = $name;
-		        $sender->sendMessage(self::PREFIX."§aYou are now vanished.");
-		        $nameTag = $sender->getNameTag();
-		        self::$nametagg[$name] = $nameTag;
-		        $sender->setNameTag("§6[VANISH]§r $nameTag");
-                if($this->getConfig()->get("enable-leave") === true){
-                    $msg = $this->getConfig()->get("FakeLeave-message");
-                    $msg = str_replace("%name", "$name", $msg);
-                    $this->getServer()->broadcastMessage($msg);
-                }
-            }else{
-                unset(self::$vanish[array_search($name, self::$vanish)]);
-                foreach($this->getServer()->getOnlinePlayers() as $players){
-                    $players->showPlayer($sender);
-                    $nameTag = self::$nametagg[$name];
-                    $sender->setNameTag("$nameTag");
-                    if($this->getConfig()->get("enable-join") === true){
-                        $msg = $this->getConfig()->get("FakeJoin-message");
-                        $msg = str_replace("%name", "$name", $msg);
-                        $this->getServer()->broadcastMessage($msg);
-                    }
-                }
-             $pk = new PlayerListPacket();
-             $pk->type = PlayerListPacket::TYPE_ADD;
-                 $pk->entries[] = PlayerListEntry::createAdditionEntry($sender->getUniqueId(), $sender->getId(), $sender->getDisplayName(), SkinAdapterSingleton::get()->toSkinData($sender->getSkin()), $sender->getXuid());
-             foreach($this->getServer()->getOnlinePlayers() as $p)
-             $p->sendDataPacket($pk);
-                $sender->sendMessage(self::PREFIX."§4You are no longer vanished!");
-            }
+				if(!in_array($name, self::$vanish)) {
+					self::$vanish[] = $name;
+					$sender->sendMessage(self::PREFIX . C::GREEN . "You are now vanished.");
+					$nameTag = $sender->getNameTag();
+					self::$nametagg[$name] = $nameTag;
+					$sender->setNameTag(C::GRAY . "[V]" . C::RESET . $nameTag);
+					if ($this->getConfig()->get("enable-leave") === true) {
+						$msg = $this->getConfig()->get("FakeLeave-message");
+						$msg = str_replace("%name", "$name", $msg);
+						$this->getServer()->broadcastMessage($msg);
+					}
+				} else {
+					unset(self::$vanish[array_search($name, self::$vanish)]);
+					foreach($this->getServer()->getOnlinePlayers() as $players) {
+						$players->showPlayer($sender);
+						$nameTag = self::$nametagg[$name];
+						$sender->setNameTag("$nameTag");
+						if($this->getConfig()->get("enable-join") === true) {
+							$msg = $this->getConfig()->get("FakeJoin-message");
+							$msg = str_replace("%name", "$name", $msg);
+							$this->getServer()->broadcastMessage($msg);
+						}
+					}
+				$pk = new PlayerListPacket();
+				$pk->type = PlayerListPacket::TYPE_ADD;
+				$pk->entries[] = PlayerListEntry::createAdditionEntry($sender->getUniqueId(), $sender->getId(), $sender->getDisplayName(), SkinAdapterSingleton::get()->toSkinData($sender->getSkin()), $sender->getXuid());
+				foreach($this->getServer()->getOnlinePlayers() as $p)
+				$p->sendDataPacket($pk);
+					$sender->sendMessage(self::PREFIX . C::DARK_RED . "You are no longer vanished!");
+				}
+			break;
         }
         return true;
     }
