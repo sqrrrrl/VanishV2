808fe0ded788d6ed2638d932cf1d2deae42eb1e7
manxy <mdemars99@gmail.com>
2020-01-05 05:48:53 -0500

    Some useless formatting and grammer fixes/changes.

--- src/superbobby/VanishV2/VanishV2Task.php
+++ src/superbobby/VanishV2/VanishV2Task.php
@@ -14,14 +14,14 @@ use function in_array;
 class VanishV2Task extends Task {
     public $pk;
 
-    public function onRun(int $currentTick){
-        foreach(Server::getInstance()->getOnlinePlayers() as $p){
-            if($p->spawned){
-                if(in_array($p->getName(), VanishV2::$vanish)){
-                    foreach(Server::getInstance()->getOnlinePlayers() as $player){
-			            if($player->hasPermission("vanish.see")){
+    public function onRun(int $currentTick) {
+        foreach(Server::getInstance()->getOnlinePlayers() as $p) {
+            if($p->spawned) {
+                if(in_array($p->getName(), VanishV2::$vanish)) {
+                    foreach(Server::getInstance()->getOnlinePlayers() as $player) {
+			            if($player->hasPermission("vanish.see")) {
 			                $player->showPlayer($p);
-		                }else{
+		                } else {
 			                $player->hidePlayer($p);
 			                $entry = new PlayerListEntry();
 			                $entry->uuid = $p->getUniqueId();
@@ -29,7 +29,7 @@ class VanishV2Task extends Task {
 			                $pk = new PlayerListPacket();
 			                $pk->entries[] = $entry;
 			                $pk->type = PlayerListPacket::TYPE_REMOVE;
-			                $player->sendDataPacket($pk);
+							$player->sendDataPacket($pk);
 		                }
                     }
                 }
