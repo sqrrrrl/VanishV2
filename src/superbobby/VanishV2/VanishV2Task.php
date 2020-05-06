3a96ef9f9fb1fbfec746eecc308e35af2661c341
sqrrl <48035361+superbobby2000@users.noreply.github.com>
2020-05-05 20:26:50 -0400

    Added a hud to remember you're vanished

--- src/superbobby/VanishV2/VanishV2Task.php
+++ src/superbobby/VanishV2/VanishV2Task.php
@@ -19,6 +19,7 @@ class VanishV2Task extends Task {
             if($p->spawned){
                 if(in_array($p->getName(), VanishV2::$vanish)){
                     foreach(Server::getInstance()->getOnlinePlayers() as $player){
+                        $p->sendPopup("§aYou are currently vanished");
 			            if($player->hasPermission("vanish.see")){
 			                $player->showPlayer($p);
 		                }else{
