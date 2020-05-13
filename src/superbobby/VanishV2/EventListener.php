92d4d8f85c9cce4a8091adf05cddc60373868392
superbobby2000 <48035361+superbobby2000@users.noreply.github.com>
2020-05-13 17:00:24 -0400

    players with vanish on can no longer pickup items

--- src/superbobby/VanishV2/EventListener.php
+++ src/superbobby/VanishV2/EventListener.php
@@ -3,7 +3,9 @@
 namespace superbobby\VanishV2;
 
 use pocketmine\event\Listener;
+use pocketmine\Server;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\inventory\InventoryPickupItemEvent;
 
 use function array_search;
 use function in_array;
@@ -15,6 +17,14 @@ class EventListener implements Listener {
         $name = $player->getName();
         if(in_array($name, VanishV2::$vanish)){
             unset(VanishV2::$vanish[array_search($name, VanishV2::$vanish)]);
+        }
+    }
+    public function PickUp(InventoryPickupItemEvent $event){
+        foreach (Server::getInstance()->getOnlinePlayers() as $p) {
+            $name = $p->getName();
+            if (in_array($name, VanishV2::$vanish)) {
+                $event->setCancelled();
+            }
         }
     }
 }
