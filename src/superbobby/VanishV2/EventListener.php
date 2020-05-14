b24137818e6cc860c728df2684935136d05d179c
sqrrl <48035361+superbobby2000@users.noreply.github.com>
2020-05-14 10:12:18 -0400

    working fr now

--- src/superbobby/VanishV2/EventListener.php
+++ src/superbobby/VanishV2/EventListener.php
@@ -12,19 +12,20 @@ use function in_array;
 
 class EventListener implements Listener {
 
-    public function onQuit(PlayerQuitEvent $event){
+    public function onQuit(PlayerQuitEvent $event) {
         $player = $event->getPlayer();
         $name = $player->getName();
-        if(in_array($name, VanishV2::$vanish)){
+        if (in_array($name, VanishV2::$vanish)) {
             unset(VanishV2::$vanish[array_search($name, VanishV2::$vanish)]);
         }
     }
-    public function PickUp(InventoryPickupItemEvent $event){
-        foreach (Server::getInstance()->getOnlinePlayers() as $p) {
-            $name = $p->getName();
-            if (in_array($name, VanishV2::$vanish)) {
-                $event->setCancelled();
-            }
+
+    public function PickUp(InventoryPickupItemEvent $event) {
+        $inv = $event->getInventory();
+        $player = $inv->getHolder();
+        $name = $player->getName();
+        if (in_array($name, VanishV2::$vanish)) {
+            $event->setCancelled();
         }
     }
 }
