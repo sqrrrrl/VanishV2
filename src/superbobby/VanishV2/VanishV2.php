0df744e9996ce5450720d1402068e4484f8a0f0f
sqrrl <48035361+superbobby2000@users.noreply.github.com>
2020-08-03 11:34:34 -0400

    New feature and customisation
    Added a command to vanish other players
    Added vanish.use.other permission
    messages are now customisable in the config file

--- src/superbobby/VanishV2/VanishV2.php
+++ src/superbobby/VanishV2/VanishV2.php
@@ -2,16 +2,14 @@
 
 namespace superbobby\VanishV2;
 
+use pocketmine\utils\TextFormat;
 use muqsit\invmenu\InvMenuHandler;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\scheduler\PluginTask;
-use pocketmine\utils\TextFormat as C;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\network\mcpe\protocol\PlayerListPacket;
 use pocketmine\network\mcpe\protocol\types\PlayerListEntry;
-use pocketmine\network\mcpe\protocol\types\SkinData;
 use pocketmine\network\mcpe\protocol\types\SkinAdapterSingleton;
 
 use function array_search;
@@ -19,7 +17,7 @@ use function in_array;
 use function strtolower;
 
 class VanishV2 extends PluginBase {
-    public const PREFIX = C::BLUE . "VanishV2 " . C::DARK_GRAY . "» ". C::RESET;
+    public const PREFIX = TextFormat::BLUE . "VanishV2 " . TextFormat::DARK_GRAY . "» ". TextFormat::RESET;
 
     public static $vanish = [];
 
@@ -42,8 +40,8 @@ class VanishV2 extends PluginBase {
         if(!InvMenuHandler::isRegistered()){
             InvMenuHandler::register($this);
         }
-        if($this->getConfig()->get("config-version") === 1){
-            $this->getLogger()->notice("Your configuration file is outdated you have to delete it to get the new update");
+        if($this->getConfig()->get("config-version") < 3 or $this->getConfig()->get("config-version") == null){
+            $this->getLogger()->error("Your configuration file is outdated you have to delete it to get the new update");
             $this->getServer()->getPluginManager()->disablePlugin($this);
         }
     }
@@ -58,68 +56,162 @@ class VanishV2 extends PluginBase {
 		case "vanish":
 		case "v":
 	        if(!$sender instanceof Player){
-                $sender->sendMessage(self::PREFIX . C::RED . "Use this command InGame.");
+                $sender->sendMessage(self::PREFIX . TextFormat::RED . "Use this command InGame.");
                 return false;
 	        }
+
+	        if(count($args) > 1){
+	            $sender->sendMessage(self::PREFIX . TextFormat::RED . "Usage: /vanish <player>");
+	            return false;
+            }
 
 	        if(!$sender->hasPermission("vanish.use")){
-		        $sender->sendMessage(self::PREFIX . C::RED . "You do not have permission to use this command");
+		        $sender->sendMessage(TextFormat::RED . "You do not have permission to use this command");
                 return false;
 	        }
 
-            if(!in_array($name, self::$vanish)){
-                self::$vanish[] = $name;
-                unset(self::$online[array_search($sender, self::$online, True)]);
-		        $sender->sendMessage(self::PREFIX . C::GREEN . "You are now vanished.");
-		        $nameTag = $sender->getNameTag();
-		        self::$nametagg[$name] = $nameTag;
-		        $sender->setNameTag("§6[V]§r $nameTag");
-                if($this->getConfig()->get("enable-leave") === true){
-                    $msg = $this->getConfig()->get("FakeLeave-message");
-                    $msg = str_replace("%name", "$name", $msg);
-                    $this->getServer()->broadcastMessage($msg);
+	        if(count($args) === 1){
+	            if(!$sender->hasPermission("vanish.use.other")){
+	                $sender->sendMessage(self::PREFIX . TextFormat::RED . "You do not have permission to vanish other players");
+	                return false;
                 }
-                    if($this->getConfig()->get("enable-fly") === true){
-                        if($sender->getGamemode() === 0){
-                        self::$AllowCombatFly[] = $name; //for BlazinFly compatibility
-                        $sender->setFlying(true);
-                        $sender->setAllowFlight(true);
+            }
+
+            if (count($args) === 0) {
+                if (!in_array($name, self::$vanish)) {
+                    self::$vanish[] = $name;
+                    unset(self::$online[array_search($sender, self::$online, True)]);
+                    $sender->sendMessage(self::PREFIX . $this->getConfig()->get("vanish-message"));
+                    $nameTag = $sender->getNameTag();
+                    self::$nametagg[$name] = $nameTag;
+                    $sender->setNameTag("§6[V]§r $nameTag");
+                    if ($this->getConfig()->get("enable-leave") === true) {
+                        $msg = $this->getConfig()->get("FakeLeave-message");
+                        $msg = str_replace("%name", "$name", $msg);
+                        $this->getServer()->broadcastMessage($msg);
+                    }
+                    if ($this->getConfig()->get("enable-fly") === true) {
+                        if ($sender->getGamemode() === 0) {
+                            self::$AllowCombatFly[] = $name; //for BlazinFly compatibility
+                            $sender->setFlying(true);
+                            $sender->setAllowFlight(true);
                         }
                     }
-                    foreach($this->getServer()->getOnlinePlayers() as $players){
-                        if($players->hasPermission("vanish.see")){
-                            $players->sendMessage(C::ITALIC . C::GRAY . "[$name: Vanished]");
+                    foreach ($this->getServer()->getOnlinePlayers() as $players) {
+                        if ($players->hasPermission("vanish.see")) {
+                            $msg = $this->getConfig()->get("vanish");
+                            $msg = str_replace("%name", "$name", $msg);
+                            $players->sendMessage($msg);
                         }
                     }
-            }else{
-                unset(self::$vanish[array_search($name, self::$vanish)]);
-                self::$online[] = $sender;
-                foreach($this->getServer()->getOnlinePlayers() as $players){
-                    $players->showPlayer($sender);
-                    $nameTag = self::$nametagg[$name];
-                    $sender->setNameTag("$nameTag");
-                    if($players->hasPermission("vanish.see")){
-                        $players->sendMessage(C::ITALIC . C::GRAY . "[$name: Unvanished]");
+                } else {
+                    unset(self::$vanish[array_search($name, self::$vanish)]);
+                    self::$online[] = $sender;
+                    foreach ($this->getServer()->getOnlinePlayers() as $players) {
+                        $players->showPlayer($sender);
+                        $nameTag = self::$nametagg[$name];
+                        $sender->setNameTag("$nameTag");
+                        if ($players->hasPermission("vanish.see")) {
+                            $msg = $this->getConfig()->get("unvanish");
+                            $msg = str_replace("%name", "$name", $msg);
+                            $players->sendMessage($msg);
+                        }
                     }
-                }
-                if($this->getConfig()->get("enable-fly") === true){
-                    if($sender->getGamemode() === 0){
-                        unset(self::$AllowCombatFly[array_search($name, self::$AllowCombatFly)]); //for BlazinFly compatibility
-                        $sender->setFlying(false);
-                        $sender->setAllowFlight(false);
+                    if ($this->getConfig()->get("enable-fly") === true) {
+                        if ($sender->getGamemode() === 0) {
+                            unset(self::$AllowCombatFly[array_search($name, self::$AllowCombatFly)]); //for BlazinFly compatibility
+                            $sender->setFlying(false);
+                            $sender->setAllowFlight(false);
+                        }
                     }
-                }
-             $pk = new PlayerListPacket();
-             $pk->type = PlayerListPacket::TYPE_ADD;
-                 $pk->entries[] = PlayerListEntry::createAdditionEntry($sender->getUniqueId(), $sender->getId(), $sender->getDisplayName(), SkinAdapterSingleton::get()->toSkinData($sender->getSkin()), $sender->getXuid());
-             foreach($this->getServer()->getOnlinePlayers() as $p)
-             $p->sendDataPacket($pk);
-		        if($this->getConfig()->get("enable-join") === true){
+                    $pk = new PlayerListPacket();
+                    $pk->type = PlayerListPacket::TYPE_ADD;
+                    $pk->entries[] = PlayerListEntry::createAdditionEntry($sender->getUniqueId(), $sender->getId(), $sender->getDisplayName(), SkinAdapterSingleton::get()->toSkinData($sender->getSkin()), $sender->getXuid());
+                    foreach ($this->getServer()->getOnlinePlayers() as $p)
+                        $p->sendDataPacket($pk);
+                    if ($this->getConfig()->get("enable-join") === true) {
                         $msg = $this->getConfig()->get("FakeJoin-message");
                         $msg = str_replace("%name", "$name", $msg);
                         $this->getServer()->broadcastMessage($msg);
-		        }
-               	    $sender->sendMessage(self::PREFIX . C::RED . "You are no longer vanished!");
+                    }
+                    $sender->sendMessage(self::PREFIX . $this->getConfig()->get("unvanish-message"));
+                }
+            }
+            if (count($args) === 1){
+                $player = $this->getServer()->getPlayer($args[0]);
+                if ($player != null) {
+                    $name = $player->getName();
+                    $othername = $sender->getName();
+                    if (!in_array($name, self::$vanish)) {
+                        self::$vanish[] = $name;
+                        unset(self::$online[array_search($player, self::$online, True)]);
+                        $msg = $this->getConfig()->get("vanish-other");
+                        $msg = str_replace("%name", "$name", $msg);
+                        $sender->sendMessage(self::PREFIX . $msg);
+                        $msg = $this->getConfig()->get("vanished-other");
+                        $msg = str_replace("%other-name", "$othername", $msg);
+                        $player->sendMessage(self::PREFIX . $msg);
+                        $nameTag = $player->getNameTag();
+                        self::$nametagg[$name] = $nameTag;
+                        $player->setNameTag("§6[V]§r $nameTag");
+                        if ($this->getConfig()->get("enable-leave") === true) {
+                            $msg = $this->getConfig()->get("FakeLeave-message");
+                            $msg = str_replace("%name", "$name", $msg);
+                            $this->getServer()->broadcastMessage($msg);
+                        }
+                        if ($this->getConfig()->get("enable-fly") === true) {
+                            if ($player->getGamemode() === 0) {
+                                self::$AllowCombatFly[] = $name; //for BlazinFly compatibility
+                                $player->setFlying(true);
+                                $player->setAllowFlight(true);
+                            }
+                        }
+                        foreach ($this->getServer()->getOnlinePlayers() as $players) {
+                            if ($players->hasPermission("vanish.see")) {
+                                $msg = $this->getConfig()->get("vanish");
+                                $msg = str_replace("%name", "$name", $msg);
+                                $players->sendMessage($msg);
+                            }
+                        }
+                    }else{
+                        unset(self::$vanish[array_search($name, self::$vanish)]);
+                        self::$online[] = $player;
+                        foreach ($this->getServer()->getOnlinePlayers() as $players) {
+                            $players->showPlayer($player);
+                            $nameTag = self::$nametagg[$name];
+                            $player->setNameTag("$nameTag");
+                            if ($players->hasPermission("vanish.see")) {
+                                $msg = $this->getConfig()->get("unvanish");
+                                $msg = str_replace("%name", "$name", $msg);
+                                $players->sendMessage($msg);                            }
+                        }
+                        if ($this->getConfig()->get("enable-fly") === true) {
+                            if ($player->getGamemode() === 0) {
+                                unset(self::$AllowCombatFly[array_search($name, self::$AllowCombatFly)]); //for BlazinFly compatibility
+                                $player->setFlying(false);
+                                $player->setAllowFlight(false);
+                            }
+                        }
+                        $pk = new PlayerListPacket();
+                        $pk->type = PlayerListPacket::TYPE_ADD;
+                        $pk->entries[] = PlayerListEntry::createAdditionEntry($player->getUniqueId(), $player->getId(), $player->getDisplayName(), SkinAdapterSingleton::get()->toSkinData($player->getSkin()), $player->getXuid());
+                        foreach ($this->getServer()->getOnlinePlayers() as $p)
+                            $p->sendDataPacket($pk);
+                        if ($this->getConfig()->get("enable-join") === true) {
+                            $msg = $this->getConfig()->get("FakeJoin-message");
+                            $msg = str_replace("%name", "$name", $msg);
+                            $this->getServer()->broadcastMessage($msg);
+                        }
+                        $msg = $this->getConfig()->get("unvanish-other");
+                        $msg = str_replace("%name", "$name", $msg);
+                        $sender->sendMessage(self::PREFIX . $msg);
+                        $msg = $this->getConfig()->get("unvanished-other");
+                        $msg = str_replace("%other-name", "$othername", $msg);
+                        $player->sendMessage(self::PREFIX . $msg);
+                    }
+                }else{
+                    $sender->sendMessage(self::PREFIX . TextFormat::RED . "Player not found");
+                }
             }
         }
         return true;
